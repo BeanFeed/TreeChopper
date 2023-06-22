b6c36aed98fad50045042e8fd03499475f605cb8
Otavio_Vittorassi <otavio.vittorassi@gmail.com>
2023-06-22 03:14:50 -0300

    Performance improvement and added ore recognition

--- src/main/java/me/otavio/treefella/listeners/BlockBreakEvent.java
+++ src/main/java/me/otavio/treefella/listeners/BlockBreakEvent.java
@@ -7,11 +7,11 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.block.Block;
-import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.inventory.meta.ItemMeta;
 
 public class BlockBreakEvent implements Listener {
@@ -23,6 +23,15 @@ public class BlockBreakEvent implements Listener {
             Material.STONE_AXE,
             Material.NETHERITE_AXE,
             Material.WOODEN_AXE
+    );
+
+    private final ImmutableList<Material> PICKAXES = ImmutableList.of(
+            Material.DIAMOND_PICKAXE,
+            Material.GOLDEN_PICKAXE,
+            Material.IRON_PICKAXE,
+            Material.STONE_PICKAXE,
+            Material.NETHERITE_PICKAXE,
+            Material.WOODEN_PICKAXE
     );
 
     @EventHandler
@@ -39,30 +48,17 @@ public class BlockBreakEvent implements Listener {
 
         boolean isPlayerPlacedBlock = PlacedBlocks.get().isInt(path);
 
-        if (AXES.contains(itemInHand.getType()) && !isPlayerPlacedBlock && p.isSneaking() && TreeFella.LOGS.contains(b.getType())) {
+        if (
+            (
+                (
+                    AXES.contains(itemInHand.getType()) && TreeFella.LOGS.contains(b.getType())
+                ) || (
+                    PICKAXES.contains(itemInHand.getType()) && TreeFella.ORES.contains(b.getType())
+                )
+            ) && !isPlayerPlacedBlock && p.isSneaking()
+        ) {
             this.checkNearbyBlocks(b, p);
 
-            boolean noMoreWood = false;
-            int yAxis = 1;
-
-            while (!noMoreWood) {
-                Location bloc = b.getLocation().clone();
-
-                bloc.add(0, yAxis, 0);
-
-                Block nextBlock = bloc.getBlock();
-
-                ItemStack currentItem = p.getInventory().getItemInMainHand();
-
-                if (TreeFella.LOGS.contains(nextBlock.getType()) && AXES.contains(currentItem.getType())) {
-                    nextBlock.breakNaturally();
-                    this.checkNearbyBlocks(nextBlock, p);
-                } else {
-                    noMoreWood = true;
-                }
-
-                yAxis++;
-            }
         } else if (isPlayerPlacedBlock && TreeFella.LOGS.contains(b.getType())) {
             PlacedBlocks.get().set(path, null);
         }
@@ -73,8 +69,7 @@ public class BlockBreakEvent implements Listener {
         int y = -1;
         int z = -1;
 
-        boolean axeIsBroken = false;
-
+        outerLoop:
         for (int k = 0; k < 3; k++) {
 
             for (int i = 0; i < 3; i++) {
@@ -86,45 +81,39 @@ public class BlockBreakEvent implements Listener {
 
                     Block nearbyBlock = center.getBlock();
 
-                    if (TreeFella.LOGS.contains(nearbyBlock.getType())) {
+                    if (TreeFella.LOGS.contains(nearbyBlock.getType()) || TreeFella.ORES.contains(nearbyBlock.getType())) {
                         nearbyBlock.breakNaturally();
 
-                        ItemStack axe = player.getInventory().getItemInMainHand();
+                        ItemStack tool = player.getInventory().getItemInMainHand();
 
-                        int axeDurability = axe.getType().getMaxDurability();
+                        int toolDurability = tool.getType().getMaxDurability();
 
-                        ItemMeta meta = axe.getItemMeta();
+                        ItemMeta meta = tool.getItemMeta();
 
                         Damageable dmg = (Damageable) meta;
 
-                        if (dmg.getDamage() <= axeDurability) {
+                        if (dmg != null && dmg.getDamage() <= toolDurability) {
                             dmg.setDamage(dmg.getDamage() + 1);
 
-                            axe.setItemMeta(dmg);
+                            tool.setItemMeta(dmg);
 
-                            player.getInventory().setItemInMainHand(axe);
+                            player.getInventory().setItemInMainHand(tool);
                         } else {
                             player.getInventory().setItemInMainHand(null);
                             player.playSound(player.getLocation(), Sound.ENTITY_ITEM_BREAK, 1F, 1F);
-                            axeIsBroken = true;
-                            break;
+                            break outerLoop;
                         }
 
                         this.checkNearbyBlocks(nearbyBlock, player);
-                    }
 
-                    if (axeIsBroken) break;
+                    }
 
                     z++;
                 }
 
-                if (axeIsBroken) break;
-
                 x++;
                 z = -1;
             }
-
-            if (axeIsBroken) break;
 
             y++;
             x = -1;
